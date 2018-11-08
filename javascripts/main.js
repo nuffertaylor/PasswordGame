b513fd0a21ac17192d699c0664f9df9b0aafb7fd
Jonas <nuffertaylor@gmail.com>
2018-11-08 18:28:45 +0000

    added js

---
+++ javascripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $("#createUser").click(function() {
+        $("#mainPage").empty();
+    });
+
+    $("#logIn").click(function() {
+        $("#mainPage").empty();
+    });
+});
