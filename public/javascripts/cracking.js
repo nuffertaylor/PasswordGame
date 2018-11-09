69401d705e9317261ebebc46bcc66530452e6ac1
Jonas <nuffertaylor@gmail.com>
2018-11-09 20:54:51 +0000

    circles update based on response from tryhack

--- public/javascripts/cracking.js
+++ public/javascripts/cracking.js
@@ -1,10 +1,10 @@
 function userToHack(user) {
+    $("#guessResponse").empty();
     $("#guessContainer").show("slow");
     //$("#whoYoureAttacking").hide("slow");
     $("#whoYoureAttacking").empty();
     $("#whoYoureAttacking").append(user);
     //$("#whoYoureAttacking").show("slow");
-
     var someJSON = new Object();
     someJSON.username = user;
     var sendJSON = JSON.stringify(someJSON);
@@ -15,6 +15,10 @@ function userToHack(user) {
         contentType: "application/json; charset=utf-8",
         success: function(data) {
             console.log("this password has " + data + " characters.");
+            for (var i = 0; i < data; i++) {
+                var html = '<i class="far fa-circle"></i>';
+                $("#guessResponse").append(html);
+            }
         }
     });
 }
@@ -28,6 +32,42 @@ $(document).ready(function() {
         }
     });
 
+    $("#submitPassword").click(function() {
+        var user = $("#whoYoureAttacking").html();
+        var pass = $("#passwordGuess").val();
+        var someJSON = new Object();
+        someJSON.username = user;
+        someJSON.password = pass;
+        var sendJSON = JSON.stringify(someJSON);
+        $.ajax({
+            url: "/tryHack",
+            type: "POST",
+            data: sendJSON,
+            contentType: "application/json; charset=utf-8",
+            success: function(data) {
+                console.log(data);
+                if (data.status == "success") {
 
-
+                }
+                else if (data.status == "wrong length") {
+                    alert("not the right length buddy");
+                }
+                else {
+                    $("#guessResponse").empty();
+                    for (var i = 0; i < data.wrong; i++) {
+                        var html = '<i class="far fa-circle"></i>';
+                        $("#guessResponse").append(html);
+                    }
+                    for (var i = 0; i < data.partial; i++) {
+                        var html = '<i class="fas fa-circle" style="color:red">';
+                        $("#guessResponse").append(html);
+                    }
+                    for (var i = 0; i < data.right; i++) {
+                        var html = '<i class="fas fa-circle" style="color:green">';
+                        $("#guessResponse").append(html);
+                    }
+                }
+            }
+        });
+    });
 });
