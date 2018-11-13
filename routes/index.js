4980a1e0b6eb904488d4b3bd93b372cdfd90dd24
Spencer Hastings EC2 <spencerhastings@byu.edu>
2018-11-13 23:06:01 +0000

    Added inc

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/userDB', { useNewUrlParser: true });
 
-var userSchema = mongoose.Schema({
+var userSchema = mongoose.Schema(
+{
     username: String,
     password: String,
     hacked: Boolean,
@@ -15,29 +16,36 @@ var User = mongoose.model('User', userSchema);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() { //Lets us know when we're connected
+db.once('open', function()
+{ //Lets us know when we're connected
     console.log('Connected');
 });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next)
+{
     res.render('index', { title: 'Express' });
 });
 
-router.post('/register', function(req, res, next) {
+router.post('/register', function(req, res, next)
+{
     console.log("POST register route");
     console.log(req.body);
     console.log(req.body.username);
     User.find({ username: req.body.username },
 
-        function(err, userList) { //Calls the find() method on your database
+        function(err, userList)
+        { //Calls the find() method on your database
             if (err) return console.error(err); //If there's an error, print it out
-            else {
-                if (userList.length) {
+            else
+            {
+                if (userList.length)
+                {
                     console.log("username taken");
                     res.json("failure");
                 }
-                else {
+                else
+                {
                     console.log(req.body.username);
 
                     var userJson = {
@@ -47,7 +55,8 @@ router.post('/register', function(req, res, next) {
                         usersHacked: 0
                     };
                     var newuser = new User(userJson);
-                    newuser.save(function(err, post) {
+                    newuser.save(function(err, post)
+                    {
                         if (err) return console.error(err);
                         console.log(post);
                         res.json("success");
@@ -59,30 +68,38 @@ router.post('/register', function(req, res, next) {
 
 });
 
-router.post('/login', function(req, res, next) {
+router.post('/login', function(req, res, next)
+{
     User.find({ username: req.body.username, password: req.body.password },
 
-        function(err, userList) { //Calls the find() method on your database
+        function(err, userList)
+        { //Calls the find() method on your database
             if (err) return console.error(err); //If there's an error, print it out
-            else {
-                if (userList.length) {
+            else
+            {
+                if (userList.length)
+                {
 
-                    if (userList[0].hacked == true) {
+                    if (userList[0].hacked == true)
+                    {
                         res.json("hacked");
                     }
-                    else {
+                    else
+                    {
                         res.json("success");
                     }
 
                 }
-                else {
+                else
+                {
                     res.json("failure");
                 }
             }
         });
 });
 
-router.post('/tryHack', function(req, res, next) {
+router.post('/tryHack', function(req, res, next)
+{
     //TODO add users username to check if they are hacked
     //  If hacked, do the account compromised popup and redirect to main screen
     //
@@ -92,51 +109,76 @@ router.post('/tryHack', function(req, res, next) {
     //
 
     User.find({ username: req.body.hackerUsername },
-        function(err, userList) { //Calls the find() method on your database
+        function(err, userList)
+        { //Calls the find() method on your database
             if (err) return console.error(err); //If there's an error, print it out
-            else {
-                if (userList.length) {
-                    if (userList[0].hacked == true) {
-                        res.json({
+            else
+            {
+                if (userList.length)
+                {
+                    if (userList[0].hacked == true)
+                    {
+                        res.json(
+                        {
                             status: "you are hacked"
                         });
                     }
-                    else {
+                    else
+                    {
                         User.find({ username: req.body.username },
-                            function(err, userList) { //Calls the find() method on your database
+                            function(err, userList)
+                            { //Calls the find() method on your database
                                 if (err) return console.error(err); //If there's an error, print it out
-                                else {
-                                    if (userList.length) {
+                                else
+                                {
+                                    if (userList.length)
+                                    {
                                         var tryPassword = req.body.password;
                                         var rightPassword = userList[0].password;
                                         console.log(tryPassword);
                                         console.log(rightPassword);
-                                        if (tryPassword.length != rightPassword.length) {
-                                            res.json({
+                                        if (tryPassword.length != rightPassword.length)
+                                        {
+                                            res.json(
+                                            {
                                                 status: "wrong length",
                                                 l: rightPassword.length
                                             });
                                         }
-                                        else if (tryPassword == rightPassword) {
-                                            User.updateOne({ username: req.body.username }, { $set: { hacked: true } }, function(err) {
+                                        else if (tryPassword == rightPassword)
+                                        {
+                                            User.updateOne({ username: req.body.username }, { $set: { hacked: true } }, function(err)
+                                            {
                                                 if (err) return console.error(err); //If there's an error, print it out
-                                                else {
-                                                    res.json({
-                                                        status: "success",
-                                                        l: rightPassword.length
+                                                else
+                                                {
+                                                    User.updateOne({ username: req.body.hackerUsername }, { $inc: {'usersHacked' : 1} }, function(err)
+                                                    {
+                                                        if (err) return console.error(err); //If there's an error, print it out
+                                                        else
+                                                        {
+                                                            res.json(
+                                                            {
+                                                                status: "success",
+                                                                l: rightPassword.length
+                                                            });
+                                                        }
                                                     });
                                                 }
                                             });
                                         }
-                                        else {
+                                        else
+                                        {
                                             var right = 0;
                                             var partial = 0;
                                             var wrong = rightPassword.length;
 
                                             //game code
 
-                                            for (var i = 0; i < rightPassword.length; i++) {
-                                                if (rightPassword.charAt(i) == tryPassword.charAt(i)) {
+                                            for (var i = 0; i < rightPassword.length; i++)
+                                            {
+                                                if (rightPassword.charAt(i) == tryPassword.charAt(i))
+                                                {
                                                     tryPassword = tryPassword.slice(0, i) + tryPassword.slice(i + 1);
                                                     rightPassword = rightPassword.slice(0, i) + rightPassword.slice(i + 1);
                                                     right++;
@@ -147,11 +189,13 @@ router.post('/tryHack', function(req, res, next) {
                                             console.log(tryPassword);
                                             console.log(rightPassword);
 
-                                            for (var i = 0; i < tryPassword.length; i++) {
+                                            for (var i = 0; i < tryPassword.length; i++)
+                                            {
                                                 var myChar = tryPassword.charAt(i);
                                                 var index = rightPassword.search(myChar);
                                                 console.log(index);
-                                                if (index > -1) {
+                                                if (index > -1)
+                                                {
                                                     tryPassword = tryPassword.slice(0, i) + tryPassword.slice(i + 1);
                                                     rightPassword = rightPassword.slice(0, index) + rightPassword.slice(index + 1);
                                                     partial++;
@@ -160,7 +204,8 @@ router.post('/tryHack', function(req, res, next) {
                                                 }
                                             }
 
-                                            res.json({
+                                            res.json(
+                                            {
                                                 status: "wrong",
                                                 right: right,
                                                 partial: partial,
@@ -168,7 +213,8 @@ router.post('/tryHack', function(req, res, next) {
                                             });
                                         }
                                     }
-                                    else {
+                                    else
+                                    {
                                         res.json("failed to find");
                                     }
                                 }
@@ -178,8 +224,10 @@ router.post('/tryHack', function(req, res, next) {
                     }
 
                 }
-                else {
-                    res.json({
+                else
+                {
+                    res.json(
+                    {
                         status: "username does not exist"
                     });
                 }
@@ -188,8 +236,10 @@ router.post('/tryHack', function(req, res, next) {
     );
 });
 
-router.post('/passLength', function(req, res, next) {
-    User.find({ username: req.body.username }, function(err, userList) {
+router.post('/passLength', function(req, res, next)
+{
+    User.find({ username: req.body.username }, function(err, userList)
+    {
         if (err) { console.log("failure"); }
         var actualpass = userList[0].password;
         var passLength = actualpass.length;
@@ -198,10 +248,13 @@ router.post('/passLength', function(req, res, next) {
 });
 
 //returns a list of only usernames
-router.get('/userList', function(req, res, next) {
-    User.find({}, function(err, userList) {
+router.get('/userList', function(req, res, next)
+{
+    User.find({}, function(err, userList)
+    {
         var allUsers = [];
-        for (var i = 0; i < userList.length; i++) {
+        for (var i = 0; i < userList.length; i++)
+        {
             var info = new Object();
             info.username = userList[i].username;
             info.hacked = userList[i].hacked;
